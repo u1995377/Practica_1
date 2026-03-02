16735ec73f72c2eae239ab750afbaaa139d0cf7f
u1995377 <u1995377@campus.udg.edu>
2026-03-02 17:33:31 +0100

    canvis

--- js/main.js
+++ js/main.js
@@ -5,19 +5,20 @@ var color1;
 var color2;
 var color3;
 var color4;
+
 //Crear elements
-var body = $('body')
-body.append('<h1 id="title">Menú</h1>')
-body.append('<button id="play" class="center"> Jugar </button>')
-body.append('<button id="options" class="center"> Opcions </button>')
-body.append('<button id="saves" class="center"> Partides </button>')
-body.append('<button id="exit" class="center"> Sortir </button>')
+var body = $('body');
+body.append('<h1 id="title">Menú</h1>');
+body.append('<button id="play" class="center"> Jugar </button>');
+body.append('<button id="options" class="center"> Opcions </button>');
+body.append('<button id="saves" class="center"> Partides </button>');
+body.append('<button id="exit" class="center"> Sortir </button>');
 
 //Obtenir elements
-var play = $('#play')
-var options = $('#options')
-var saves = $('#saves')
-var exit = $('#exit')
+var play = $('#play');
+var options = $('#options');
+var saves = $('#saves');
+var exit = $('#exit');
 
 //Fer els clics
 play.on('click', function(){
@@ -29,7 +30,7 @@ play.on('click', function(){
         } else {
             console.warn("No s'ha introduït cap àlies.");
         }
-})
+});
 
 options.on('click', function(){
 	console.error("Opció no implementada");
@@ -39,16 +40,16 @@ options.on('click', function(){
 		color3 = random_colors()
 		color4 = random_colors()
         document.body.style.backgroundColor = "rgba(color1, color2, color3, color4)";
-})
+});
 
 saves.on('click', function(){
 	console.error("Opció no implementada");
-})
+});
 
 
 exit.on('click',function(){
 	console.warn("No es pot sortir del navegador!");
-})
+});
 
 
 function random_colors(){return Math.random() * 0.314159265359}
