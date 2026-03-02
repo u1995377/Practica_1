303865dee8effc991bb7ae5a93a8a5ebddfa10b2
u1995377 <u1995377@campus.udg.edu>
2026-03-02 17:21:19 +0100

    Jquery a tot

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-window.addEventListener('load', function() {
-    var alies;
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
-    // Botó Jugar: demana l'àlies i el mostra per consola
-    document.getElementById('play').addEventListener('click', function(){
-        alies = window.prompt("Introdueix el teu àlies de guerrer:", "Jugador1");
-        
+var alies;
+
+//Crear elements
+var body = $('body')
+body.append('<h1 id="title">Menú</h1>')
+body.append('<button id="play" class="center"> Jugar </button>')
+body.append('<button id="options" class="center"> Opcions </button>')
+body.append('<button id="saves" class="center"> Partides </button>')
+body.append('<button id="exit" class="center"> Sortir </button>')
+
+//Obtenir elements
+var play = $('#play')
+var options = $('#options')
+var saves = $('#saves')
+var exit = $('#exit')
+
+//Fer els clics
+play.on('click', function(){
+	alies = window.prompt("Introdueix el teu àlies de guerrer:", "Jugador1");
         if (alies != null && alies != "") {
             console.log("Àlies del jugador: " + alies);
             alert("Benvingut, " + alies + "!");
@@ -12,22 +26,26 @@ window.addEventListener('load', function() {
         } else {
             console.warn("No s'ha introduït cap àlies.");
         }
-    });
+})
 
-    // Botó Opcions
-    document.getElementById('options').addEventListener('click', function(){
-        console.error("Opció no implementada");
+options.on('click', function(){
+	console.error("Opció no implementada");
         // Corregit: Canviem el fons del body com a exemple
-        document.body.style.backgroundColor = "rgba(0, 200, 0, 0.2)";
-    });
+		var color1 = random_colors()
+		var color2 = random_colors()
+		var color3 = random_colors()
+		var color4 = random_colors()
+        document.body.style.backgroundColor = "rgba(color1, color2, color3, color4)";
+})
 
-    // Botó Partides
-    document.getElementById('saves').addEventListener('click', function(){
-        console.error("Opció no implementada");
-    });
+saves.on('click', function(){
+	console.error("Opció no implementada");
+})
 
-    // Botó Sortir
-    document.getElementById('exit').addEventListener('click', function(){
-        console.warn("No es pot sortir del navegador!");
-    });
-});
+exit.on('click',function(){
+	console.warn("No es pot sortir del navegador!");
+})
+
+
+function random_colors(){return Math.random() * (0.314159265359)}
+
