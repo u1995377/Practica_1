0acd4d84f8aed9fc47103de32a9a393315fefbae
u1995377 <u1995377@campus.udg.edu>
2026-04-22 01:43:38 +0200

    canviar a fitxer selectGame

--- js/SelectGame.js
+++ js/SelectGame.js
@@ -1,9 +1,24 @@
 import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
-$('#gamemode1').on('click', function(){
-    location.assign("./game.html");
+function prepareGame(mode) {
+    // 1. Netegem partides anteriors
+    sessionStorage.removeItem('load');
+
+    // 2. Agafem el valor directament (serà "2", "3" o "4")
+    const groupSize = $('#group').val();
+
+    // 3. Guardem a la sessió
+    sessionStorage.setItem('mode', mode);
+    sessionStorage.setItem('groupSize', groupSize);
+
+    // 4. Anem al joc
+    window.location.assign("./game.html");
+}
+
+$('#gamemode1').on('click', function() {
+    prepareGame('1');
 });
 
-$('#gamemode2').on('click', function(){
-    location.assign("../");
+$('#gamemode2').on('click', function() {
+    prepareGame('2');
 });
